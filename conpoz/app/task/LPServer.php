22b75de42700ef30b904a2675412e713a9731a78
Hikaru Chang <kensou0012@gmail.com>
2017-07-04 20:49:13 +0800

    LPServer don't use LPCenter

--- conpoz/app/task/LPServer.php
+++ conpoz/app/task/LPServer.php
@@ -7,7 +7,7 @@ class LPServer
     {
         $port = $this->app->argv[2];
         // $l = new \Conpoz\App\Lib\LPServer\Listener(array('port' => $port, 'allowOrigin' => '*', 'keepAlive' => true, 'hashKey' => $bag->config->LPServer['hashKey'], 'centerHost' => '127.0.0.1', 'centerPort' => '50000'));
-        $l = new \Conpoz\App\Lib\LPServer\Listener(array('port' => $port, 'allowOrigin' => '*', 'keepAlive' => true, 'centerHost' => '127.0.0.1', 'centerPort' => '50000'));
+        $l = new \Conpoz\App\Lib\LPServer\Listener(array('port' => $port, 'allowOrigin' => '*', 'keepAlive' => true, 'hashKey' => $bag->config->LPServer['hashKey']));
         $l->dispatch();
     }
     
